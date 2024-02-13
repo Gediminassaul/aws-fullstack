6a1ac337ef10bfb74a2be0ff27a79d3ca3ee4552
gediminas.s <gediminas.s@8devices.com>
2024-02-13 18:02:19 +0200

    done cognito authentification

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsfullstackcb44f313": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
